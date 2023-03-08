8a9553a4840b1682e8f31751b78c9748cb9a1593
Shahzad Naeem <shahzad65naeem@gmail.com>
2023-03-08 10:40:54 +0000

    Better string handling. STOP

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ use serde_json::Value;
 pub enum PathArg {
     Rdi,
     Rsi,
+    RsiCount,
 }
 
 pub fn get_command() -> Command {
@@ -64,37 +65,47 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let res = waitpid(child_pid, None)?;
     eprintln!("first wait: {:?}", res.yellow());
 
-    let mut is_sys_exit = false;
-
     loop {
+        // Syscall entry trace
         ptrace::syscall(child_pid, None)?;
         _ = waitpid(child_pid, None)?;
-        if is_sys_exit {
-            if let Ok(regs) = ptrace::getregs(child_pid) {
-                let file_arg_no = syscall_table[&regs.orig_rax].1;
 
-                let path = if file_arg_no == Some(PathArg::Rdi) {
-                    get_string_from_addr(child_pid, regs.rdi)
-                } else if file_arg_no == Some(PathArg::Rsi) {
-                    get_string_from_addr(child_pid, regs.rsi)
-                } else {
-                    format!("")
-                };
+        // Syscall exit trace
+        ptrace::syscall(child_pid, None)?;
+        _ = waitpid(child_pid, None)?;
 
-                eprintln!(
-                    "{}({:x}, {:x}{}, {:x}, ...) = {:x}",
-                    syscall_table[&regs.orig_rax].0.green(),
-                    regs.rdi.blue(),
-                    regs.rsi.blue(),
-                    path.green(),
-                    regs.rdx.blue(),
-                    regs.rax.yellow(),
-                );
+        if let Ok(regs) = ptrace::getregs(child_pid) {
+            let file_arg_no = syscall_table[&regs.orig_rax].1;
+
+            let path = if file_arg_no == Some(PathArg::Rdi) {
+                get_string_from_addr(child_pid, regs.rdi, None)
+            } else if file_arg_no == Some(PathArg::Rsi) {
+                get_string_from_addr(child_pid, regs.rsi, None)
+            } else if file_arg_no == Some(PathArg::RsiCount) {
+                get_string_from_addr(child_pid, regs.rsi, Some(regs.rdx as usize))
             } else {
-                break;
-            }
+                format!("")
+            };
+
+            let res = regs.rax as i64;
+            let status = if res >= -1024 && res <= 1024 {
+                format!("{}", res)
+            } else {
+                format!("0x{:x}", res)
+            };
+
+            eprintln!(
+                "{}({:x}, {:x}{}, {:x}, ...) = {}",
+                syscall_table[&regs.orig_rax].0.green(),
+                regs.rdi.blue(),
+                regs.rsi.blue(),
+                path.green(),
+                regs.rdx.blue(),
+                status.yellow(),
+            );
+        } else {
+            break;
         }
-        is_sys_exit = !is_sys_exit;
     }
 
     Ok(())
@@ -113,15 +124,21 @@ pub fn build_syscall_table() -> HashMap<u64, (String, Option<PathArg>)> {
         .map(|item| {
             let mut idx: Option<PathArg> = None;
 
-            // TODO: Not all strings are NULL terminated - eg read has a size_t count param following the string
-
             let filename_arg_idx = if let Value::Array(ary) = item {
                 if let Some(Value::String(v)) = ary[RDI_IDX].get("type") {
                     if v.contains("char __user *") {
                         idx = Some(PathArg::Rdi);
                     } else if let Some(Value::String(v)) = ary[RSI_IDX].get("type") {
                         if v.contains("char __user *") {
-                            idx = Some(PathArg::Rsi);
+                            if let Some(Value::String(v)) = ary[RSI_IDX + 1].get("type") {
+                                if v == "size_t count" {
+                                    idx = Some(PathArg::RsiCount);
+                                } else {
+                                    idx = Some(PathArg::Rsi);
+                                }
+                            } else {
+                                idx = Some(PathArg::Rsi);
+                            }
                         }
                     }
                 }
@@ -141,8 +158,8 @@ pub fn build_syscall_table() -> HashMap<u64, (String, Option<PathArg>)> {
     syscall_table
 }
 
-pub fn get_string_from_addr(pid: Pid, addr: u64) -> String {
-    const MAX_STRING_LEN: usize = 48;
+pub fn get_string_from_addr(pid: Pid, addr: u64, max_len: Option<usize>) -> String {
+    const MAX_STRING_LEN: usize = 100;
 
     let full_path: String;
     let mut addr = addr;
@@ -153,27 +170,51 @@ pub fn get_string_from_addr(pid: Pid, addr: u64) -> String {
         let w1 = ptrace::read(pid, addr as *mut c_void);
 
         if let Ok(pp) = w1 {
+            let mut done = false;
+
             let mut bytes = pp
                 .to_le_bytes()
                 .into_iter()
                 .take_while(|b| *b != 0u8)
                 .collect::<Vec<_>>();
 
+            if let Some(limit) = max_len {
+                let num_bytes = bytes.len();
+                let num_all_bytes = all_bytes.len();
+
+                if num_all_bytes + num_bytes >= limit {
+                    let excess_bytes = num_all_bytes + num_bytes - limit;
+
+                    if excess_bytes > 0 {
+                        bytes = bytes[0..(num_bytes - excess_bytes)].to_vec();
+                    }
+
+                    // Get rid of any trailing newline before output
+                    if bytes[bytes.len() - 1] == '\n' as u8 {
+                        bytes.remove(bytes.len() - 1);
+                    }
+
+                    done = true;
+                }
+            }
+
             let num_bytes = bytes.len();
 
             all_bytes.append(&mut bytes);
 
-            addr += mem::size_of::<u64>() as u64;
+            if all_bytes.len() > MAX_STRING_LEN {
+                let mut ddd = " ...".as_bytes().to_vec();
+                all_bytes.append(&mut ddd);
 
-            if num_bytes < mem::size_of::<u64>() || all_bytes.len() > MAX_STRING_LEN {
-                if num_bytes == mem::size_of::<u64>() {
-                    let mut dot_dot_dot = " ...".as_bytes().to_vec();
-                    all_bytes.append(&mut dot_dot_dot);
-                }
+                done = true;
+            }
 
+            if num_bytes < mem::size_of::<u64>() || done {
                 full_path = String::from_utf8_lossy(&all_bytes).to_string();
                 break;
             }
+
+            addr += mem::size_of::<u64>() as u64;
         } else {
             full_path = format!("FILE_PATH - Error :( {:x}", addr);
             break;
